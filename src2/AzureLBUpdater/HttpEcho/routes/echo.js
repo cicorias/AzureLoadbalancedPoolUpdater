8921ade25c09b505a5237b9b2360abf45bd066a5
Shawn Cicoria <github@cicoria.com>
2015-11-24 18:03:23 -0500

    added echo and start of py

---
+++ src2/AzureLBUpdater/HttpEcho/routes/echo.js
@@ -0,0 +1,16 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.send('respond with a resource');
+});
+
+
+router.post('/', function (req, res) {
+    res.send(req.body);
+});
+
+module.exports = router;
+
+
